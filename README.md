e3db880b819564125a937491d6252038d6fe1c9f
Dustin Haffner <dh@xix.org>
2019-08-08 00:23:45 +0000

    Update example5

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ $ python example1.py
 $ python example2.py
 [...]
 ```
+
+## Transformation example
+```
+$ python -m __transformers__ print example5.py
+$ python -m __transformers__ run example5.py
+```
--- example5.py
+++ example5.py
@@ -10,8 +10,9 @@ h = 1 + ...
 # Curry a keyword argument
 formatter = "Pycon Africa 2019, {name}!".format(name=...)
 
-lst = list(range(10))
+lst = list(range(1000))
 sort_lst_with_key = sorted(lst, key=...)
 
 # Curry a list comprehension
 get_elements_less_than = [x for x in lst if x < ...]
+print(get_elements_less_than(20))
